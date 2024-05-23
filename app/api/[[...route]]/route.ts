592a3bc9cad0e902f9335e4933741a1b636c2569
FinnedSun <135132984+FinnedSun@users.noreply.github.com>
2024-05-23 18:37:20 +0700

    summary API + Rp currency

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
+import summary from "./summary";
 import accounts from './accounts';
 import categories from './categories';
 import transactions from './transactions';
@@ -11,9 +12,10 @@ export const runtime = 'edge';
 const app = new Hono().basePath('/api')
 
 const routes = app
-    .route("/accounts", accounts)
-    .route("/categories", categories)
-    .route("/transactions", transactions)
+  .route("summary", summary)
+  .route("/accounts", accounts)
+  .route("/categories", categories)
+  .route("/transactions", transactions)
 
 export const GET = handle(app)
 export const POST = handle(app)
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { eachDayOfInterval, isSameDay } from "date-fns"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -15,9 +16,55 @@ export function convertAmountToMiliunits(amount: number) {
 
 //Penjelasan ada di 7:27
 export function formatCurrency(value: number) {
-  return Intl.NumberFormat('en-US', {
+  return Intl.NumberFormat('id-ID', {
     style: 'currency',
-    currency: "USD",
-    minimumFractionDigits: 2,
+    currency: "IDR",
+    minimumFractionDigits: 3,
   }).format(value)
+}
+
+export function calculcatePrecentageChange(
+  current: number,
+  previus: number
+) {
+  if (previus === 0) {
+    return previus === current ? 0 : 100
+  }
+
+  return ((current - previus) / previus) * 100
+}
+
+export function fillMissingDays(
+  activeDays: {
+    date: Date,
+    income: number,
+    expenses: number
+  }[],
+  startDate: Date,
+  endDate: Date
+) {
+  if (activeDays.length === 0) {
+    return []
+  }
+
+  const allDays = eachDayOfInterval({
+    start: startDate,
+    end: endDate,
+  })
+
+  const transactionsByDay = allDays.map((day) => {
+    const found = activeDays.find((d) => isSameDay(d.date, day))
+
+    if (found) {
+      return found
+    } else {
+      return {
+        date: day,
+        income: 0,
+        expenses: 0
+      }
+    }
+  })
+
+  return transactionsByDay
 }
