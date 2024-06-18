3e7e467d54a4e5566e0129e7792290315832cefe
Finned sundew <finned@srv535741.hstgr.cloud>
2024-06-18 09:28:07 +0000

    pg update

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,6 +1,8 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
+// import { drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from './schema';
+import postgres from 'postgres'
 
 export const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql, { schema });
+export const db = drizzle(sql, { schema, logger: true });
