e20250c0ff18a716bef2ec96f1f8206f43497756
FinnedSun <rayhanagungmaulana.04@gmail.com>
2024-10-23 21:35:30 +0700

    fix typo jumlah

--- app/(dashboard)/transactions/columns.tsx
+++ app/(dashboard)/transactions/columns.tsx
@@ -107,7 +107,7 @@ export const columns: ColumnDef<ResponseType>[] = [
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          Jumalah
+          Jumlah
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       )
--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,17 @@ import { NextResponse } from "next/server";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-    '/',
+  '/',
 ]);
 
 export default clerkMiddleware((auth, request) => {
-    if (isProtectedRoute(request)) {
-        auth().protect()
-    }
+  if (isProtectedRoute(request)) {
+    auth().protect()
+  }
 
-    return NextResponse.next()
+  return NextResponse.next()
 });
 
 export const config = {
-    matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
